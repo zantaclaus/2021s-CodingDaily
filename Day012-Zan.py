f865618a2527e6f7585e26dfda6e4a37cd3193ae
Tarzuto <tarzuto@gmail.com>
2021-01-12 18:04:53 +0700

    Biggest between index 0 and index 2

--- Day012-Zan.py
+++ Day012-Zan.py
@@ -1,36 +1,7 @@
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# d = int(input())
-# e = int(input())
-# value = a + b + c + d + e
-
-# check = 0
-# lenght = 0
-# lst = []
-# for i in range(0, 5):
-#     lst.append(int(input()))
-#     value = value + lst[i]
-#     lenght += 1
-
-# for i in range(0,5):
-#     if(value - lst[i] > 0):
-#         check += 1
-# if(check == lenght):
-#     print("True")
-# else:
-#     print("False")
-value = 0
-count = 0
 lst = [int(item) for item in input().split()]
-for item in lst:  
-    value += item
+big = max(lst[0], lst[2])
+lst[0] = big
+lst[1] = big
+lst[2] = big
 
-for item in lst:
-    if value - item > item:
-        count += 1
-
-if(count == len(lst)):
-    print("True")
-else:
-    print("False")
+print(lst)
