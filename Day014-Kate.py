87af849b38927812fd74de9264b6954a77bea2a6
Tarzuto <tarzuto@gmail.com>
2021-01-14 17:26:27 +0700

    Nested List

---
+++ Day014-Kate.py
@@ -0,0 +1,6 @@
+#Jan 14, 2021- find a maximum sum of the elements in the nested list
+# ex. input [[1,2,3],[3,2,1]]
+# output should be 6 cause the max sum in the nested list is 6(1+2+3) and 6(3+2+1)
+# ex2. input [[1,5], [7,3], [3,5]] output should be 10(7+3)
+
+
