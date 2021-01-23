18f8f0aed67dd5a78ab5e0ef178c18322e48e18f
Tarzuto <tarzuto@gmail.com>
2021-01-23 15:13:28 +0700

    Search index

--- Day022-Kate.py
+++ Day022-Kate.py
@@ -1,1 +1,11 @@
-# Binary search
+def searchIndex(lst, n):
+    index = 0
+    for item in lst:
+        if item == n:
+            return index
+        index += 1
+    return -1
+
+lst = [1,2,3,4,5]
+n = 5
+print(searchIndex(lst,n))
