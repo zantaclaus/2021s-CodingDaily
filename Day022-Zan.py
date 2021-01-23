7a61e3db88aeda08e829153db87abc9bd7d76294
Tarzuto <tarzuto@gmail.com>
2021-01-23 14:51:09 +0700

    Text Wrap

--- Day022-Zan.py
+++ Day022-Zan.py
@@ -1,1 +1,10 @@
-# text Wrap
+string = input()
+n = int(input())
+
+count = 0
+for item in string:
+    count += 1
+    print(item, end="")
+    if count == n:
+        count =  0
+        print("")
